2e6b78e570dfe67fab34d027fb0499c1e32fc09b
Denis Akira <denis_akira@hotmail.com>
2019-05-19 22:30:30 -0300

    alterando docker e deletando files nao mais utilizados

--- database/migrations/2016_01_01_000000_add_voyager_user_fields.php
+++ database/migrations/2016_01_01_000000_add_voyager_user_fields.php
@@ -13,7 +13,10 @@ class AddVoyagerUserFields extends Migration
             if (!Schema::hasColumn('users', 'avatar')) {
                 $table->string('avatar')->nullable()->after('email')->default('users/default.png');
             }
-            $table->integer('role_id')->nullable()->after('id');
+
+            if (!Schema::hasColumn('users', 'role_id')) {
+                $table->integer('role_id')->nullable()->after('id');
+            }
         });
     }
 
